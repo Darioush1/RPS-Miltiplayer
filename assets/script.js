07810b3b23e9a26d0c1aa2359a570ddb7c084ea4
Darioush Mansourzadeh <darioush2011@gmail.com>
2019-08-18 16:27:11 -0700

    button logic working

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+var userInput = '';
+var userInput2 = '';
+
+function pickR() {
+    console.log('Player picks rock')
+};
+
+function pickP(){
+    console.log('Player picks paper')
+};
+
+function pickS( ){
+    console.log('Player picks scissors')
+};
+
+$('.playRock').on('click', function(){
+    pickR()
+});
